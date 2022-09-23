70bc171a3d91cadacb3f2f889336afc85ef663fe
north-tower <muneshmyke@gmail.com>
2022-09-23 12:13:43 +0300

    Creating React App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from './components/NavBar';
-import Body from './components/Body';
-import SearchBar from './components/Search';
+import Weather from './components/Weather';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
 
 
   return (
     <div>
-      <NavBar  / >
-        
-      <Body / > 
-
+      <NavBar />
+      <Weather />
     </div>
       );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.css";
-import Search from "./Search" ;
-
 
 function NavBar() {
     return( 
       <div>
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            
-            <div class="container-fluid">
-              
-                    
-              <div class="collapse navbar-collapse justify-content-center"
-                id="navbarCenteredExample"
-              >
-                
+            <div class="container-fluid">                    
+              <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
                 <ul class="navbar-nav mb-2 mb-lg-0">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Tiempo Aplicación</a>
                   </li>
-                </ul>
-                
+                </ul>  
               </div>
-              
             </div>
-            
           </nav>
       </div>
     )}
